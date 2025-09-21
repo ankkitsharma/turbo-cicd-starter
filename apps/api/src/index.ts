b219e4650fe36b5593b44c2a3aeadbc6827170c3
ankkitsharma <ankitsharma2801@gmail.com>
2025-09-21 21:03:39 +0530

    add api

---
+++ apps/api/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const port = 3001;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+
